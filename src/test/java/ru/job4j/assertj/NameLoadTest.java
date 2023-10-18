1468e586d96baa3a58b3670659a87cb5b91a40da
emelkin-pro <emelkin-pro@yandex.ru>
2023-10-18 10:06:39 +0300

    4. Утверждения с исключениями

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkEmptyName() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::parse)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("Names array is empty");
+    }
+    @Test
+    void checkNameWithoutSignEqual() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "key:value";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(name)
+                .hasMessageContaining( "this name: %s does not contain the symbol '='".formatted(name));
+    }
+
+    @Test
+    void checkNameWithoutKey() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "=kek";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(name)
+                .hasMessageContaining( "this name: %s does not contain a key".formatted(name));
+    }
+
+    @Test
+    void checkNameWithoutValue() {
+        NameLoad nameLoad = new NameLoad();
+        String name = "lol=";
+        assertThatThrownBy(() -> nameLoad.parse(name))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(name)
+                .hasMessageContaining( "this name: %s does not contain a value".formatted(name));
+    }
+}
