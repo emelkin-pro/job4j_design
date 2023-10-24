2ebc2797824788aae3e0c741ea4c16752ab48680
emelkin-pro <emelkin-pro@yandex.ru>
2023-10-24 07:35:07 +0300

    3. Итератор четных чисел

--- src/test/java/ru/job4j/iterator/EvenNumbersIteratorTest.java
+++ src/test/java/ru/job4j/iterator/EvenNumbersIteratorTest.java
@@ -1,8 +1,10 @@
 package ru.job4j.iterator;
 
 import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.util.*;
 
 class EvenNumbersIteratorTest {
@@ -10,7 +12,7 @@ class EvenNumbersIteratorTest {
 
     @BeforeEach
     public void setUp() {
-        iterator = new EvenNumbersIterator(new int[] {1, -3, 2, 3, 5, 5, -4, 5, 6, 7});
+        iterator = new EvenNumbersIterator(new int[]{1, -3, 2, 3, 5, 5, -4, 5, 6, 7});
     }
 
     @Test
@@ -36,20 +38,20 @@ class EvenNumbersIteratorTest {
     }
 
     @Test
-    void  shouldReturnFalseIfNoAnyEvenNumbers() {
+    void shouldReturnFalseIfNoAnyEvenNumbers() {
         iterator = new EvenNumbersIterator(new int[]{1});
         assertThat(iterator.hasNext()).isFalse();
     }
 
     @Test
-    void  shouldReturnFalseIfNoAnyNumbers() {
+    void shouldReturnFalseIfNoAnyNumbers() {
         iterator = new EvenNumbersIterator(new int[]{});
         assertThat(iterator.hasNext()).isFalse();
     }
 
     @Test
     void allNumbersAreEven() {
-        iterator = new EvenNumbersIterator(new int[] {2, 4, 6, 8});
+        iterator = new EvenNumbersIterator(new int[]{2, 4, 6, 8});
         assertThat(iterator.hasNext()).isTrue();
         assertThat(iterator.next()).isEqualTo(2);
         assertThat(iterator.hasNext()).isTrue();
@@ -63,7 +65,7 @@ class EvenNumbersIteratorTest {
 
     @Test
     void allNumbersAreOdd() {
-        iterator = new EvenNumbersIterator(new int[] {1, 3, 5, 7});
+        iterator = new EvenNumbersIterator(new int[]{1, 3, 5, 7});
         assertThat(iterator.hasNext()).isFalse();
         assertThat(iterator.hasNext()).isFalse();
     }
