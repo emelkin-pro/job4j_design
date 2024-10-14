59acdb66a8fbb743ae6dbf690655642d03533de2
emelkin-pro <emelkin-pro@yandex.ru>
2024-10-14 14:52:22 +0300

    9.0. Файлы и директории. Класс File

---
+++ src/main/java/ru/job4j/io/Search.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Search {
+    public static void main(String[] args) throws IOException {
+        Path start = Paths.get(".");
+        search(start, path -> path.toFile().getName().endsWith(".js")).forEach(System.out::println);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) {
+        SearchFiles searcher = new SearchFiles(condition);
+        try {
+            Files.walkFileTree(root, searcher);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return searcher.getPaths();
+    }
+}
