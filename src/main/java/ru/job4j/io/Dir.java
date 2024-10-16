0f1e3fde1d14f88da49fa71b4f8ac0e34a7eec26
emelkin-pro <emelkin-pro@yandex.ru>
2024-10-16 12:11:04 +0300

    12. Валидация параметров запуска

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -5,21 +5,22 @@ import java.util.Objects;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("/Users/user/IdeaProjects/job4j_design");
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
+        File file = new File(args[0]);
+
         if (!file.exists()) {
             throw new IllegalArgumentException(
-                    String.format("Директория не существует: %s", file.getAbsoluteFile()));
+                    String.format("Not exist %s", file.getAbsoluteFile()));
         }
         if (!file.isDirectory()) {
             throw new IllegalArgumentException(
-                    String.format("Это не директория: %s", file.getAbsoluteFile()));
+                    String.format("Not directory %s", file.getAbsoluteFile()));
         }
+        System.out.printf("size : %s%n", file.getTotalSpace());
         for (File subfile : Objects.requireNonNull(file.listFiles())) {
-            if (subfile.isFile()) {
-                System.out.println(subfile.getName() + " - " + subfile.length());
-            } else if (subfile.isDirectory()) {
-                System.out.println(subfile.getName() + " - " + subfile.getTotalSpace());
-            }
+            System.out.println(subfile.getName());
         }
     }
 }
