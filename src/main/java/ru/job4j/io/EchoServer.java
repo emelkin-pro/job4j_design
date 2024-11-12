159ff5ecc5f9637fd8d86f970ca1e7e3410c50d3
emelkin-pro <emelkin-pro@yandex.ru>
2024-11-12 10:10:08 +0300

    3. Бот

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -34,7 +34,6 @@ public class EchoServer {
                         output.write("What\r\n\r\n".getBytes());
                         output.flush();
                     }
-
                     for (String string = input.readLine();
                          string != null && !string.isEmpty();
                          string = input.readLine()) {
